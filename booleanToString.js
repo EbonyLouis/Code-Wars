269ff210ce74c1f7e1a02d39727f835ae56f0bab
Ebony Louis <55366651+EbonyLouis@users.noreply.github.com>
2019-11-01 18:28:48 -0400

    Add files via upload

---
+++ booleanToString.js
@@ -0,0 +1,9 @@
+
+// Instructions: In this programming exercise, you're going
+// to learn about functions, boolean (true/false) values,
+// strings, and the if-statement.
+
+
+function booleanToString(b){
+  return b.toString()
+}
