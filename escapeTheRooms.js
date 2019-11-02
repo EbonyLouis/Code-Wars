60c95ea26c7fb7b8f505db80202dbcb32809eabd
Ebony Louis <55366651+EbonyLouis@users.noreply.github.com>
2019-11-02 14:57:25 -0400

    Add files via upload

---
+++ escapeTheRooms.js
@@ -0,0 +1,27 @@
+// Instructions:
+// You are creating an escape the room game. The first step is create an object
+// called rooms that contains all of the rooms of the game. The room object should
+// have at least 3 rooms as properties, and each individual room should have at least
+// three properties (example: name, description, completed).
+
+var rooms = {
+  room1: {
+    name: 'room1',
+    descriptions: 'green',
+    completed: 'yes'
+
+  },
+  room2:{
+    name: 'room2',
+    descriptions: 'purple',
+    completed: 'no'
+
+  },
+
+  room3:{
+    name: 'room3',
+    descriptions: 'blue',
+    completed: 'yes'
+
+  }
+}
