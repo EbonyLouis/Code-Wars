c718507549c33ea80521bae9f6de689184ec1bd0
Ebony Louis <55366651+EbonyLouis@users.noreply.github.com>
2019-11-16 21:50:30 -0500

    Add files via upload

---
+++ sumOfCubes.js
@@ -0,0 +1,12 @@
+// Instructions: Write a function that takes a positive integer n,
+// sums all the cubed values from 1 to n, and returns that sum.
+//
+// Assume that the input n will always be a positive integer.
+
+function sumCubes(n){
+let sumOfCubes = 0
+for(let i =1;i<=n; i++){
+sumOfCubes += i * i * i
+}
+return sumOfCubes
+}
