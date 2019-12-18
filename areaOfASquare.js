9b0a69dbe399078ed2b478ef39c4d78f847ff2be
Ebony Louis <55366651+EbonyLouis@users.noreply.github.com>
2019-12-17 21:29:49 -0500

    Create areaOfASquare.js

---
+++ areaOfASquare.js
@@ -0,0 +1,8 @@
+INSTRUCTIONS: Complete the function that calculates the area of the red square, when the length of the circular 
+arc A is given as the input. Return the result rounded to two decimals.
+
+
+function squareArea(a){
+  let r= (a*2)/(Math.PI)
+  return Math.round(Math.pow(r,2)*100)/100
+}
